e70e7242ea4caf19f72b19d03f9ec38fe876f3b7
overthestream <njhnjh02@naver.com>
2023-10-29 20:29:26 +0900

    feat

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
-@Controller('/greetings')
+@Controller('/users')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+  // parameter query
+  // GET /users/:id /users/1
+  @Get('/:index')
+  getUsernameByIndex(@Param('index') index: number, @Res() res: Response) {
+    try {
+      const username = this.appService.getUsernameByIndex(index);
+      res.send(username);
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  }
+
+  @Post('/:newusername')
+  postUsernameByIndex(
+    @Param('newusername') newUsername: string,
+    @Res() res: Response,
+  ) {
+    try {
+      const newuser = this.appService.postUsernameByIndex(newUsername);
+      res.status(201).send(newuser);
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,23 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  usernames: string[];
+  constructor() {
+    this.usernames = ['노정훈', '권예진'];
+  }
+
   getHello(): string {
     return 'Hello World!';
+  }
+  // Index
+  getUsernameByIndex(index: number) {
+    if (index < 0 || index >= this.usernames.length) {
+      throw Error('Index out of range');
+    }
+    return this.usernames[index];
+  }
+
+  postUsernameByIndex(newusername: string) {
+    this.usernames.push(newusername);
   }
 }
